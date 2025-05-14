8656eebc359c6dd36262ca1368831533ffaa86d0
AniketKSingh <aniket20092001@gmail.com>
2025-05-14 22:38:06 +0530

    completed Lecture 41

---
+++ Lecture_41/index.js
@@ -0,0 +1,63 @@
+// console.log(10+20);
+// let a = 20;
+// console.log(a);
+// console.log(++a);
+// console.log(a++);
+
+// assingment operator
+// let a = 10;
+// a+=20;
+// console.log(a);
+
+// == & ====
+// let b = 7;
+// let c = '7';
+// console.log(b == c);
+// console.log(b === c);
+
+// Ternary Operator
+// 10 > 20 ? console.log("true") : console.log("false");
+
+// console.log(true && true && false);
+// console.log(!true);
+// console.log(true || false);
+
+// console.log(false && 0);
+// console.log(true && 3);
+// console.log(true && " ");
+
+// console.log(false || true || false || true || false);
+// console.log(false && true || false || true || false);
+// console.log(7 || 11 || 18 || 21);
+
+// Bitwise operator
+// console.log(2 | 5);
+// console.log(2 & 5);
+// console.log(~0);
+// console.log(2 ^ 2);
+// console.log(1 << 1);
+// console.log(5 << 2);
+// console.log(20 >> 2);
+
+
+let a = 10;
+let b = 20;
+
+if(a < b){
+    console.log(b);
+}
+else{
+    console.log(b);
+}
+
+let c = 3;
+
+switch(c){
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('F');
+}
