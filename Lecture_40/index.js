68bd14fbabb82ef8b0cf5fab306431b441078178
AniketKSingh <aniket20092001@gmail.com>
2025-05-14 19:59:22 +0530

    Lecture 40 basics of data types

---
+++ Lecture_40/index.js
@@ -0,0 +1,34 @@
+// console.log(10);
+
+// var a = 10;
+// console.log(a);
+// {
+//     console.log(a);
+// }
+// {
+//     let v = 15;
+//     console.log(v);
+//     v =10;
+//     console.log(v);
+// }
+// // console.log(v);
+// const p = 25;
+// console.log(p);
+
+// let int = 32543543541.2125435;
+// console.log(typeof(int));
+
+// let pnq = 3546384354748143743476743712717437437434949464642745767675757676427676767676727376737579673427374657167627132711712163172671957152716276179471967162741961759716297147167171213542313212667619741631731732715271671715763715671486425478743541351397412676769167151771671619771671671571.4357;
+// console.log(typeof(pnq));
+// console.log(pnq);
+
+let marks = 20.9;
+console.log(typeof(marks));
+marks= "babbar";
+console.log(typeof(marks));
+marks = "new";
+console.log(typeof(marks));
+marks = null;
+console.log(typeof(marks));
+
+
