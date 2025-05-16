28d663d892f950cfb9354ea30615b9376e240958
AniketKSingh <aniket20092001@gmail.com>
2025-05-16 19:42:13 +0530

    Lecture 42 completed basics of string and loops

---
+++ Lecture_42/index.js
@@ -0,0 +1,66 @@
+// for(let i = 0; i < 10; i++){
+//    console.log(i);
+//    console.log(" ");
+// }
+
+// let c = 3;
+// switch(c){
+//     case 1: console.log('A');
+//     break;
+//     case 2: console.log('B');
+//     break;
+//     case 3: console.log('C');
+//     break;
+//     default: console.log('F');
+// }
+
+// let i = 5;
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+// let p = 10;
+// do{
+//     console.log(p);
+//     p--;
+// }while(p > 0);
+
+// let firstName = "Love";
+// let lastName = "babbar";
+
+// console.log(firstName + lastName);
+
+// let names = `love 
+// babbar
+// this 
+// is it`;
+// console.log(names);
+
+let op1 = "love";
+let op2 = "babbar";
+
+// let ans = op1 + op2;
+// console.log(ans);
+
+// console.log(op1+op2);
+
+// console.log(`${op1}${op2}`);
+// let c = `${op1}${op2}`;
+// console.log(c);
+
+// console.log(op1.toUpperCase());
+// console.log(op2.toLowerCase());
+
+// console.log(op1.length);
+
+let a = "babbar";
+console.log(a.substring(2));
+console.log(a.substring(2,5));
+
+let b = "This is Love Babbar";
+
+let words = b.split(' ');
+console.log(words);
+
+console.log(words.join('-'));
