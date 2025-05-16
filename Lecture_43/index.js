5c71eb76f0fa06715afeebc400b91e00a3f1929c
AniketKSingh <aniket20092001@gmail.com>
2025-05-16 23:15:25 +0530

    Lecture 43 completed basic of Functions

---
+++ Lecture_43/index.js
@@ -0,0 +1,39 @@
+// function sayMyName(){
+//     console.log("Love Babbar");
+// }
+
+// sayMyName();
+
+// function printNumber(num){
+//     console.log("Primary Number:",num);
+// }
+// printNumber(5);
+
+// function getAverage(num1, num2){
+//     let avg = (num1 + num2);
+//     console.log("avg:", avg);
+// }
+
+// getAverage(50, 21);
+
+// function getSum(a,b,c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+
+// let ans = getSum(10,12,5);
+// console.log("Sum", ans);
+
+// function getName(firstName, lastName){
+//     let fullName = firstName + " " + lastName;
+//     return fullName;
+// }
+
+// console.log(getName("Love", "Babbar"));
+
+let sum = (a,b) =>{
+    let ans = a + b;
+    return ans;
+}
+
+console.log(sum(10,5));
