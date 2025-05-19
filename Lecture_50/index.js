0cd549de24f4b8d222c472f953b55dbd3e7eafb5
AniketKSingh <aniket20092001@gmail.com>
2025-05-19 18:34:08 +0530

    Lecture 50 Error handling

---
+++ Lecture_50/index.js
@@ -0,0 +1,44 @@
+// try{
+//     console.log(x);
+// }
+// catch(err){
+//     console.log("Phle define kr le bhai", err);
+// }
+
+// try{
+//     console.log('A');
+//     // console.log(x);
+//     console.log('B');
+//     console.log(y);
+//     console.log('C');
+// }
+// catch(err){
+//     console.log("bhai define krle");
+// }
+// finally{
+//     console.log("will run");
+// }
+
+// try{
+//     console.log('A');
+//     let errorCode = 100;
+
+//     if(errorCode == 100){
+//         throw new Error("Invalid Json");
+//     }
+// }
+// catch(e){
+
+// }
+// try{
+//     console.log(x);
+// }
+// catch(err){
+//     throw new Error("Invalid Json");
+// }
+
+let errorCode = 100;
+if(errorCode == 100){
+    throw new console.error("Invalid Json");
+    
+}
